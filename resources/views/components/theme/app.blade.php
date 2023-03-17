ba6e1468ea001ea581fb4b3dfb3b34c9807f1b8b
aldmf26 <aldimf26@gmail.com>
2023-03-17 14:50:35 +0800

    1

--- resources/views/components/theme/app.blade.php
+++ resources/views/components/theme/app.blade.php
@@ -1,28 +1,34 @@
 @props([
-'title' => '',
+    'title' => '',
+    'table' => 'Y',
+    'sizeCard' => '12',
 ])
 <x-theme.head :title="$title" />
 
 <x-theme.navbar />
 
 <div class="content-wrapper container">
-
-
-    <div class="page-content card">
-        <div class="card-header">
-            <h3 class="float-start">{{$title}}</h3>
-            {{$buttonHeader}}
-
+    <div class="page-content">
+        @if ($table == 'T')
+            {{ $slot }}
+        @else
+        <div class="row">
+            <div class="col-lg-{{$sizeCard}}">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="float-start">{{ $title }}</h3>
+                        {{ $cardHeader }}
+                    </div>
+                    <div class="card-body">
+    
+                        {{ $cardBody }}
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="card-body">
-            <section class="row">
-                {{ $isi }}
-            </section>
-        </div>
+        @endif
 
     </div>
-
-</div>
 </div>
 
 <x-theme.footer />
