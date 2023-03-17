063ee4bbd3532c3cff053dbbfc05febf4d766593
nadws <nandw567@gmail.com>
2023-03-17 17:05:40 +0800

    dsda

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\KaryawanController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Routing\RouteGroup;
 use Illuminate\Support\Facades\Route;
 
